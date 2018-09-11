bcf5959af51ac9e82b67aea5b4c1c4f5c774ac94
joshua-hurn <joshua.hurn@hotmail.com>
2018-09-11 10:42:29 -0500

    final push

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let friends = ['Harry', 'Ron', 'Hermoine', 'Dumbledore', 'Snape'];
 
 for (let i = 0; i < friends.length; i++) {
-    for (let j = 100;j < 100; j--);
+    console.log(friends[i])
+    for (let j = 99; j >= 1; j--) {
+        if (j > 1) {
+            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, 99 lines of code in the file")
+        } else
+            console.log("1 line of code in the file, 1 line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file")
+    }
 }
-
-
-
-console.log(j)
