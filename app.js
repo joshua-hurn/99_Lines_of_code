27df3fddef0da8cf0a0fc2575e21f5e2359f13fd
joshua-hurn <joshua.hurn@hotmail.com>
2018-09-11 11:25:50 -0500

    Working Final

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ for (let i = 0; i < friends.length; i++) {
     console.log(friends[i])
     for (let j = 99; j >= 1; j--) {
         if (j > 1) {
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, 99 lines of code in the file")
+            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file")
         } else
             console.log("1 line of code in the file, 1 line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file")
     }
